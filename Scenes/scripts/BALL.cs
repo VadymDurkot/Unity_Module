96f57ffef5ee9c0f9ee09cc6fc1d1f42eb3c0ca6
VadymDurkot <57210295+VadymDurkot@users.noreply.github.com>
2021-11-19 15:16:33 +0200

    Add files via upload

---
+++ Scenes/scripts/BALL.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerScript : MonoBehaviour
+{
+
+    public float playerVelocity;
+    private Vector3 playerPosition;
+
+    // используйте этот метод для инициализации
+    void Start()
+    {
+        // получим начальную позицию платформы
+        playerPosition = gameObject.transform.position;
+    }
+
+    // Update вызывается при отрисовке каждого кадра игры
+    void Update()
+    {
+        // горизонтальное движение
+        playerPosition.x += Input.GetAxis("Horizontal") * playerVelocity;
+
+        // выход из игры
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Application.Quit();
+        }
+
+        // обновим позицию платформы
+        transform.position = playerPosition;
+    }
+}
